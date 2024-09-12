0e9c19f3332daf816ca6efcdae529c43d5e02dd3
Andrew-S-Nelson <andrewnelson2596@gmail.com>
2024-09-12 10:48:17 -0400

    Create nc_tcp_scan.sh

---
+++ Scan_Scripts/nc_tcp_scan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Enter network address (e.g. 192.168.0): "
+read net
+echo "Enter starting host range (e.g. 1): "
+read start
+echo "Enter ending host range (e.g. 254): "
+read end
+echo "Enter ports space-delimited (e.g. 21-23 80): "
+read ports
+for ((i=$start; $i<=$end; i++))
+do
+    nc -nvzw1 $net.$i $ports 2>&1 | grep -E "succ|open"
+done
