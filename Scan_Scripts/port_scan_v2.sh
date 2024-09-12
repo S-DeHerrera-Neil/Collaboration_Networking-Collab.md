112ed44556ad1759f3a9ed713f262d78dddc65d4
Andrew-S-Nelson <andrewnelson2596@gmail.com>
2024-09-12 12:56:54 -0400

    Update port_scan_v2.sh

--- Scan_Scripts/port_scan_v2.sh
+++ Scan_Scripts/port_scan_v2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "scanning ${1} on ports ${2} thru ${3}" > scan_result
+# Command Syntax ./port_scan_v2.sh X.X.X.X start_port end_port
+echo "scanning ${1} on ports ${2} thru ${3}"
 for ((i=${2};i<=${3};i++)); do
 	timeout 0.5 echo -n 2>/dev/null < /dev/tcp/${1}/${i} && echo "port ${i} open" >> scan_result || echo "port ${i} closed" >> scan_result &
 	if [[ $(expr ${i} % 1000) -eq 0 ]]; then
