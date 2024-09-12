035944be817c865b9d7806f5276e02991ee63602
Andrew-S-Nelson <andrewnelson2596@gmail.com>
2024-09-12 19:02:42 -0400

    Update ping_sweep_v2.sh

--- Scan_Scripts/ping_sweep_v2.sh
+++ Scan_Scripts/ping_sweep_v2.sh
@@ -5,10 +5,10 @@ echo "ping scan for ${i}.1-255" > ping_result
 for i in {1..255} ; do ping -c 3 -l 3 ${1}.$i >> ping_result & done
 echo "ping_sweep_v2"
 echo -n "loading"
-sleep 1s
+sleep 0.3s
 echo -n ". "
-sleep 1s
+sleep 0.3s
 echo -n ". "
-sleep 1s
+sleep 0.3s
 echo ". "
 cat ping_result | grep -P -B 1 -e '[1-9] received' | grep -P -o -e '(\d{1,3}\.){3}\d{1,3}' | sort
